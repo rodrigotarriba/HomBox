1a60106d1db4985c751b1be30ed3621f7dd449f8
Rod_2 <r@r.com>
2022-12-08 22:25:28 -0800

    Added digital twin and rotation controllers

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Transform[] allChildrenTransforms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplyTransformations();
+            
+    }
+
+    void ApplyTransformations(){
+
+        if (Input.GetAxis("Fire1") != 0 && (Input.GetAxis("Fire2") != 0))
+        {
+            foreach(var children in allChildrenTransforms)
+            {
+                Vector3 tempPos = children.position;
+                Quaternion tempRot = children.rotation;
+                children.SetParent(null);
+                children.position = tempPos;
+                children.rotation = tempRot;
+            }
+
+        }
+
+
+
+
+            if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.position = new Vector3(transform.position.x - .001f, transform.position.y, transform.position.z);
+        }
+        else if(Input.GetAxis("Horizontal") < 0)
+        {
+            transform.position = new Vector3(transform.position.x + .001f, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - .001f);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + .001f);
+        }
+
+
+        if (Input.GetAxis("Fire3") > 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - .001f, transform.position.z);
+        }
+        else if (Input.GetAxis("Jump") > 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + .001f, transform.position.z);
+        }
+
+            if (Input.GetAxis("Fire1") > 0)
+        {
+            transform.Rotate(new Vector3(0f, -.1f, 0f));
+        }
+
+        if (Input.GetAxis("Fire2") > 0)
+        {
+            transform.Rotate(new Vector3(0f, +.1f, 0f));
+        }
+
+
+
+
+    }
+}
